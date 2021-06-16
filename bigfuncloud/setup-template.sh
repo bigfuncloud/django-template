00063af54005242b531a9d0a4a7d5d8ac5ec34a8
Tim Mickel <tim@tmickel.com>
2021-06-16 12:54:54 -0400

    setup-template: need python

--- bigfuncloud/setup-template.sh
+++ bigfuncloud/setup-template.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+set -ex
+
+direnv allow
+eval "$(direnv export bash)"
+
 SECRET=$(python -c "import secrets; print(secrets.token_urlsafe())")
 echo "DJANGO_SECRET_KEY=\"$SECRET\"" > .envrc.secret
